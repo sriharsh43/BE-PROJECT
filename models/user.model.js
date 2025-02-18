34971e69d1c1124dc4416c5baa87ffcfaa2f3eca
sriharsh43 <sriharsh543@gmail.com>
2025-02-18 21:36:12 +0530

    methods

--- models/user.model.js
+++ models/user.model.js
@@ -57,5 +57,24 @@ userSchema.pre("save",async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
     await bcrypt.compare(password,this.password)
 }
+userSchema.methods.generateAcessToken = function () {
+    jwt.sign({
+        _id : this._id,
+        username : this.username,
+        email : this.email,
+        fullname : this.fullname
+    },process.env.ACCESS_TOKEN_SECRET),
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+}
+userSchema.methods.generateRefreshAcessToken = function () {
+    jwt.sign({
+        _id : this._id,
+    },process.env.REFRESH_TOKEN_SECRET),
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+}
 
 export const User = mongoose.model("User",userSchema)
