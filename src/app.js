9661a7e26f2e1a964e26be9b2420e755865c0489
sriharsh43 <sriharsh543@gmail.com>
2025-02-18 16:41:12 +0530

    utils

--- src/app.js
+++ src/app.js
@@ -5,17 +5,19 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN,
+    origin : process.env.CORS_ORIGIN, // cors
     credentials : true
 }))
 
 app.use(express.json({
-    limit : "16kb"
+    limit : "16kb"  //this is the json limit
 }))
+//Returns middleware that only parses urlencoded bodies and only looks at requests where the Content-Type header matches the type option
 app.use(express.urlencoded({
     extended : true,
     limit : "16kb"
 }))
+//It serves static files and is based on serve-static.
 app.use(express.static("public"))
 app.use(cookieParser())
 export {app}
