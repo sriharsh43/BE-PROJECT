b717dfdbc968bb262ebfb1ae7f8a553a557a2ed1
sriharsh43 <sriharsh543@gmail.com>
2025-02-19 17:34:57 +0530

    multer

--- utils/fileUpload.js
+++ utils/fileUpload.js
@@ -6,3 +6,29 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET 
 });
 
+    const uploadOnCloudinary = async (localFilePath) =>{
+        try {
+            if(!localFilePath) return null
+            //upload on cloudinary
+            const response = await cloudinary.uploader.upload(localFilePath,{
+                resource_type : 'auto'
+            })
+            //file has been uploaded
+            console.log("File is uploaded on cloudinary");
+            console.log(response.url);
+            return response
+            
+            b hi 
+        } catch (error) {
+            fs.unlink(localFilePath) //remove the locally saved temporary file as cloud upload operation failed
+            return null
+        }
+    }
+    export{uploadOnCloudinary}
+
+/*cloudinary.v2.uploader.upload('https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg',{ public_id: 'shoes'},
+    function (error,result) {
+        console.log(result)})*/
+       
+    
+    
